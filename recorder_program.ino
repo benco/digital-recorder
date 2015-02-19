69d961043f3834954ef54a9ad733f7ec489f10f4
unknown <bpc92989@JD1607-01.csun.edu>
2015-02-18 19:07:22 -0800

    Added the LAME library and put the code I wrote up.

---
+++ recorder_program.ino
@@ -0,0 +1,162 @@
+/*-----------------------------------------------------------*
+ |                                                           |
+ | Senior Design - ECE 493                       Spring 2015 |
+ | Digital Sound Recorder                                    |
+ |                                                           |
+ *-----------------------------------------------------------*
+ | File: recorder_code.ino                                   |
+ | Description: Contains code for the control operation and  |
+ |              recording on the Arduino Uno microcontroller |
+ |                                                           |
+ *-----------------------------------------------------------*
+ | Changelog:                                                |
+ | [2/11/15] Implemented the block diagram on the drive      |
+ |                                                           |
+ *-----------------------------------------------------------*
+ | Todo:                                                     |
+ | - Figure out how to access SD card files                  |
+ | - Make a recording routine to be called by a timer int    |
+ | - Have serial messages to debug stuff                     |
+ |                                                           |
+ *-----------------------------------------------------------*/
+
+#include <SD.h>
+#include <configMS.h>
+
+// LED connection pin numbers: digital output
+const int FileLED = 13;
+const int RecordLED = 12;
+// Speaker: analog data written as PWM output 
+const int SpeakerPin = 10;
+// Button pins: digital input, debounced by ISR
+const int PlayButton = 7;
+const int RecordButton = 6;
+const int DeleteButton = 5;
+const int InterruptPin = 2;  // Triggers ISR for all buttons
+// Debouncing array
+volatile int BufferArray = 0;
+// Representing button conditions
+const int PLAY = 0xFFFF;
+const int RECORD = 0xAAAA;
+const int DELETE = 0x5555;
+// Nice constants
+const int PLAY = 3;
+const int RECORD = 2;
+const int DELETE = 1;
+// State variables
+bool haveFile = true; 
+bool isPlaying = false;
+bool isRecording = false;
+
+
+// When any button is pressed, 2 go low and this
+//  method will continually run until all buttons
+//  are released.
+// Debounces by shifting the button # into a volatile
+//  array, which is checked later.
+void buttonISR() {
+  int latestButton = 0;
+  // Determine which button pressed, then
+  //  get lowest two bits of the associated
+  //  32-bit (or however long ints are) condition. 
+  if (digitalRead(PlayButton)) latestButton = PLAY & 3; 
+  else if (digitalRead(RecordButton)) latestButton = RECORD & 2;
+  else if (digitalRead(DeleteButton)) latestButton = DELETE & 1;
+  else latestButton = 0;
+  // Shift in that binary value
+  BufferArray = (BufferArray << 2) | latestButton;
+}
+
+
+// Initializing...
+void setup() {
+  // Serial setup for debugging statements
+  Serial.begin(9600); 
+  // LED controls & turn them all off (assume active high)
+  pinMode(FileLED, OUTPUT); 
+  pinMode(RecordLED, OUTPUT); 
+  digitalWrite(FileLED, LOW);
+  digitalWrite(RecordLED, LOW);
+
+  // Initialize SD shield
+  // while (don't have SD card)
+  ///  wait
+  ///  blink a light quickly
+  // Get file system access
+  // if (we have "recording.mp3")
+  ///  haveFile = true
+  ///  FileLED on
+  
+  // Button inputs
+  pinMode(PlayButton, INPUT);
+  pinMode(RecordButton, INPUT);
+  pinMode(DeleteButton, INPUT);
+  pinMode(InterruptPin, INPUT);
+  
+  // Activate button-sensing ISR
+  /// When pin 2 goes low, the button ISR
+  /// will run and check when button we have
+  attachInterrupt(0, buttonISR, LOW);
+  
+}
+
+// We check if the button was pressed by
+//  seeing if the whole buffer if full of
+//  that button's 2-bit number.
+bool isPressed(button) {
+  if (BufferArray == button) {
+    BufferArray = 0;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void loop() {
+  if (isPressed(PLAY)) {
+    if (haveFile) {
+      digitalWrite(PlayLED, HIGH); // PlayLED on
+      isPlaying = true;
+      while (isPlaying) {
+        // play a little
+        isPlaying = !isPressed(PLAY);
+        Serial.print("playing...");
+      }
+      digitalWrite(PlayLED, LOW); // PlayLED off
+    } else {
+      Serial.print("NO FILE");
+      // CONTINUE
+    }
+  } else if (isPressed(RECORD)) {
+    if (!haveFile) {
+      digitalWrite(RecordLED, LOW); // RecordLED on
+      isRecording = true;
+      while (isRecording) {
+        // record a little (MESHAL)
+        isRecording = !isPressed(RECORD);
+        Serial.print("recording...");
+      }
+      digitalWrite(RecordLED, LOW); // RecordLED off
+      haveFile = true;
+      digitalWrite(FileLED, HIGH); // FileLED on
+      // create file...?
+      // CONTINUE
+    } else {
+      // Blink angrily (MESHAL)
+      Serial.print("WE ALREADY HAVE A FILE");
+      // CONTINUE
+    }
+  } else if (isPressed(DELETE)) {
+    if (haveFile) {
+      // delete the file somehow
+      haveFile = false;
+      digitalWrite(FileLED, LOW); // FileLED off
+    } else {
+      // CONTINUE
+    }
+  } else {
+    // No buttons were pressed. GREAT!
+    // CONTINUE
+  }
+  delay(100);
+}
