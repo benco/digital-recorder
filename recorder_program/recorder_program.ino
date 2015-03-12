d273a7a4fa1c4bc4b3c22e2be4c4080c71cea88c
Ben Companeitz <Ben@benpro.local>
2015-03-11 21:38:33 -0700

    Can access files on the SD card (outputs text at beginning), when you press the play button, it outputs a sine wave from a table to the speaker pin so that we can test maximum frequency that the arduino can produce. It might be too small.

--- recorder_program/recorder_program.ino
+++ recorder_program/recorder_program.ino
@@ -40,7 +40,7 @@ const int PLAY = 0xFFFF;
 const int RECORD = 0xAAAA;
 const int DELETE = 0x5555;
 // State variables
-bool haveFile = true; 
+bool haveFile = true; // SHOULD START OUT FALSE? 
 bool isPlaying = false;
 bool isRecording = false;
 bool quitRecord = false;
@@ -65,6 +65,19 @@ void buttonISR() {
   // Shift in that binary value
   BufferArray = (BufferArray << 2) | latestButton;
 }
+
+// Table for sine-output tester!!!
+int sine[] = { 127, 134, 142, 150, 158, 166, 173, 181, 188, 195,
+201, 207, 213, 219, 224, 229, 234, 238, 241, 245,
+247, 250, 251, 252, 253, 254, 253, 252, 251, 250,
+247, 245, 241, 238, 234, 229, 224, 219, 213, 207,
+201, 195, 188, 181, 173, 166, 158, 150, 142, 134,
+127, 119, 111, 103, 95, 87, 80, 72, 65, 58,
+ 52, 46, 40, 34, 29, 24, 19, 15, 12, 8,
+  6, 3, 2, 1, 0, 0, 0, 1, 2, 3,
+  6, 8, 12, 15, 19, 24, 29, 34, 40, 46,
+ 52, 58, 65, 72, 80, 87, 95, 103, 111, 119,
+127 };
 
 
 // Initializing...
@@ -97,6 +110,9 @@ void setup() {
     Serial.println("error opening file!");
   }
   
+  // Setup speaker PWM output frequency to 31372.55
+  TCCR2B = TCCR2B & 0b11111000 | 0x01; 
+  
   // if (we have "recording.mp3")
   ///  haveFile = true
   ///  FileLED on
@@ -126,6 +142,21 @@ bool isPressed(int button) {
   }
 }
 
+// ASUMPTION: b has length of >= n!
+long readSample(int n, File f) {
+  unsigned long sample = 0;
+  if (f.available() >= n) {
+    for (int i=0; i<n; i++) {
+      Serial.print(((unsigned)f.read()) << (8*i));
+      Serial.print("\t");
+      sample = sample + (((unsigned)f.read()) << (8*i));
+    }
+  }
+  Serial.print(sample);
+  Serial.print("\t");
+  return sample;
+}
+
 void loop() {
   if (isPressed(PLAY)) {
     while (isPressed(PLAY)) {} //Will start playing when button released (Hanger)
@@ -135,12 +166,36 @@ void loop() {
       quitPlaying = false;
       
       byte data = 0;
-      myFile = SD.open("knight_rider_access.wav", FILE_READ);
+      myFile = SD.open("sine/20000hz.wav", FILE_READ);
+      //myFile = SD.open("krider.wav", FILE_READ);
+      //myFile = SD.open("nocomp.wav", FILE_READ);
+      //myFile = SD.open("ateam.wav", FILE_READ);
+      //myFile = SD.open("test.txt", FILE_READ);
       if (myFile) {
-        for (int i=0; i<64; i++) {
+        // print out the header
+        for (int i=0; i<44; i++) {
           Serial.print(myFile.read());
         }
-        Serial.println("");
+        // play the audio, assuming 24-bit
+        unsigned long sample = 0;
+        
+        int count = 0;
+        boolean derp = true;
+        
+        while (myFile.available()) {
+         //sample = (readSample(3, myFile) * 255) / 65536;
+         //Serial.println(sample);
+         //analogWrite(SpeakerPin, sample);
+         
+         // SINE
+         analogWrite(SpeakerPin, sine[count]);
+         count = (count+1)%101;
+         
+         // SQUARE
+         //if (derp) analogWrite(SpeakerPin, 200);
+         //else analogWrite(SpeakerPin, 0);
+         //derp = !derp;
+        }
         delay(10000);
       } else {
         Serial.println("WTFFFF MANNNN");
@@ -245,3 +300,7 @@ void loop() {
   }
   //delay(100);
 }
+
+
+
+
