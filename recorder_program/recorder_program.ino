776c21b81ee4a6a7372655c52edbc995e9edbd04
Ben Companeitz <Ben@benpro.local>
2015-03-30 19:49:56 -0700

    Audio ISR works so far, buttons remapped to analog pins, SD card reading doesn't hang (not sure when simultaneous with Timer2 though...), rearranged some stuff for clarity.

--- recorder_program/recorder_program.ino
+++ recorder_program/recorder_program.ino
@@ -12,39 +12,42 @@
  | Changelog:                                                |
  | [2/11/15] Implemented the block diagram on the drive      |
  | [3/??/15] Added test signal output code in play block     |
+ | [3/30/15] Rearranged stuff, buttons read through analog   |
+ |           pins, Audio ISR works                           |
  |                                                           |
  *-----------------------------------------------------------*
  | Todo:                                                     |
- | - Figure out how to access SD card files                  |
- | - Make a recording routine to be called by a timer int    |
- | - Have serial messages to debug stuff                     |
+ | - Read into a big-ass circular buffer from the SD card    |
+ | - Maybe a structure for the circular buffer?              |
+ | - Reave the chaff!                                        |
  |                                                           |
  *-----------------------------------------------------------*/
 #include <SD.h>
 //#include <configMS.h> //what is this??
 
+// Representing button conditions
+#define PLAY 0xFFFF
+#define RECORD 0xAAAA
+#define DELETE 0x5555
+
 // LED connection pin numbers: digital output
-const int FileLED = 10;
-const int RecordLED = 6; //9 works fine
-const int PlayLED = 7;
+#define FileLED 10
+#define ActiveLED 9
 // Speaker: analog data written as PWM output 
 //          NEW: configure digital port!
+//          SUPER NEW: WE'RE USING PORTD DOWG
 const int APORT[] = {3, 3, 3, 3, 3, 3, 3, 3}; 
 const int SpeakerPin = 5;
 const int MicrophonePin = A0;
 // Button pins: digital input, debounced by ISR
-const int PlayButton = 9;
-const int RecordButton = 8;
-const int DeleteButton = 0;
-const int InterruptPin = 2;  // Triggers ISR for all buttons
+#define PlayButton A0
+#define RecordButton A1
+#define DeleteButton A2
+#define InterruptPin 2  // Triggers ISR for all buttons
 // Debouncing array
-volatile int BufferArray = 0;
-// Representing button conditions
-const int PLAY = 0xFFFF;
-const int RECORD = 0xAAAA;
-const int DELETE = 0x5555;
+volatile int ButtonBuffer = 0;
 // State variables
-bool haveFile = true; // SHOULD START OUT FALSE? 
+bool haveFile = true; // SHOULD START OUT FALSE? (yes, but nice for test)
 bool isPlaying = false;
 bool isRecording = false;
 bool quitRecord = false;
@@ -57,46 +60,55 @@ File myFile;
 //  are released.
 // Debounces by shifting the button # into a volatile
 //  array, which is checked later.
-void buttonISR() {
-  int latestButton = 0;
+int latestButton = 0;
+#define ButtonLow 90
+void BUTTON_ISR() {
+  //int latestButton = 0;
   // Determine which button pressed, then
   //  get lowest two bits of the associated
   //  32-bit (or however long ints are) condition. 
-  if (!digitalRead(PlayButton)) latestButton = PLAY & 3; 
-  else if (!digitalRead(RecordButton)) latestButton = RECORD & 3;
-  else if (!digitalRead(DeleteButton)) latestButton = DELETE & 3;
+  if (analogRead(PlayButton)<ButtonLow) latestButton = PLAY & 3; 
+  else if (analogRead(RecordButton)<ButtonLow) latestButton = RECORD & 3;
+  else if (analogRead(DeleteButton)<ButtonLow) latestButton = DELETE & 3;
   else latestButton = 0;
   // Shift in that binary value
-  BufferArray = (BufferArray << 2) | latestButton;
+  ButtonBuffer = (BufferArray << 2) | latestButton;
 }
-
-// Table for sine-output tester!!!
-int sine[] = { 127, 134, 142, 150, 158, 166, 173, 181, 188, 195,
-201, 207, 213, 219, 224, 229, 234, 238, 241, 245,
-247, 250, 251, 252, 253, 254, 253, 252, 251, 250,
-247, 245, 241, 238, 234, 229, 224, 219, 213, 207,
-201, 195, 188, 181, 173, 166, 158, 150, 142, 134,
-127, 119, 111, 103, 95, 87, 80, 72, 65, 58,
- 52, 46, 40, 34, 29, 24, 19, 15, 12, 8,
-  6, 3, 2, 1, 0, 0, 0, 1, 2, 3,
-  6, 8, 12, 15, 19, 24, 29, 34, 40, 46,
- 52, 58, 65, 72, 80, 87, 95, 103, 111, 119,
-127 };
 
 
 // Initializing...
 void setup() {
   // Serial setup for debugging statements
   Serial.begin(9600); 
-
+	Serial.println("WHAT THE FUCK!");
   // LED controls & turn them all off (assume active high)
   pinMode(FileLED, OUTPUT); 
-  pinMode(RecordLED, OUTPUT); 
-  pinMode(PlayLED, OUTPUT);
-  digitalWrite(PlayLED, LOW);
+  pinMode(ActiveLED, OUTPUT); 
   digitalWrite(FileLED, LOW);
-  digitalWrite(RecordLED, LOW);
+  digitalWrite(ActiveLED, LOW);
+
+	//// SPEAKER SETUP ///////////////////////////////////////////////////
+	
+  // NEW audio handler using built-in support
+	cli(); // disables interrupts
+	TIMSK2 = 0x0;
+	//set timer2 interrupt at 8kHz
+	TCCR2A = 0;// set entire TCCR2A register to 0
+  TCCR2B = 0;// same for TCCR2B
+  TCNT2  = 0;//initialize counter value to 0
+  // set compare match register for 40khz increments
+  OCR2A = 49;// = (16*10^6) / (40000*8) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR2A |= (1 << WGM21);
+  // Set CS21 bit for 8 prescaler
+  TCCR2B |= (1 << CS21);   
+  // enable timer compare interrupt
+  //TIMSK2 |= (1 << OCIE2A);
+	TIMSK2 = 0x0;
+  sei();//enable interrupts
   
+	//////////////////////////////////////////////////////////////////////
+
 	// Initialize SD shield
 	pinMode(4, OUTPUT);
   if (!SD.begin(4)) {
@@ -104,7 +116,6 @@ void setup() {
   } else {
   	Serial.println("SD init done");
 	}
-  
   // TESTING THE SD CARD ACCESS
   myFile = SD.open("test.txt", FILE_READ);
   //myFile = SD.open("timemachine.wav", FILE_READ);
@@ -117,54 +128,49 @@ void setup() {
   } else {
     Serial.println("error opening file!");
   }
-  
-	//// SPEAKER SETUP ///////////////////////////////////////////////////
-	
-  // OLD Setup speaker PWM output for pin 5 to 62500Hz
-  TCCR0B = TCCR0B & 0b11111000 | 0x01;
-  
-	// Setup pins for DIGITAL audio port
-	for (int i=0; i<sizeof(APORT); i++) {
-		pinMode(APORT[i], OUTPUT);
-	}
-	digitalWrite(APORT[0], LOW);
-
-  // NEW audio handler using built-in support
-	cli(); // disables interrupts
-	//set timer2 interrupt at 8kHz
-	TCCR2A = 0;// set entire TCCR2A register to 0
-  TCCR2B = 0;// same for TCCR2B
-  TCNT2  = 0;//initialize counter value to 0
-  // set compare match register for 40khz increments
-  OCR2A = 49;// = (16*10^6) / (40000*8) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR2A |= (1 << WGM21);
-  // Set CS21 bit for 8 prescaler
-  TCCR2B |= (1 << CS21);   
-  // enable timer compare interrupt
-  TIMSK2 |= (1 << OCIE2A);
-  sei();//enable interrupts
-  
-	//////////////////////////////////////////////////////////////////////
-
   // if (we have "recording.mp3")
   ///  haveFile = true
   ///  FileLED on
   
-  // Button inputs
-  pinMode(PlayButton, INPUT);
-  pinMode(RecordButton, INPUT);
-  pinMode(DeleteButton, INPUT);
-  pinMode(InterruptPin, INPUT);
+	// set PORTD as output, except interrupt pin 2
+	DDRD = 0xFB;
   
   // Activate button-sensing ISR
   /// When pin 2 goes low, the button ISR
   /// will run and check when button we have
-  attachInterrupt(0, buttonISR, LOW);
+  attachInterrupt(0, BUTTON_ISR, LOW);
   
 }
 
-volatile unsigned int buffer[256];
+//volatile unsigned int buffer[256];
+volatile unsigned int buffer[] = {
+128, 131, 134, 137, 141, 144, 147, 150, 153, 156,
+159, 162, 165, 168, 171, 174, 177, 180, 183, 186,
+189, 191, 194, 197, 199, 202, 205, 207, 209, 212,
+214, 217, 219, 221, 223, 225, 227, 229, 231, 233,
+235, 236, 238, 240, 241, 243, 244, 245, 246, 248,
+249, 250, 251, 252, 252, 253, 254, 254, 255, 255,
+255, 256, 256, 256, 256, 256, 256, 256, 255, 255,
+254, 254, 253, 253, 252, 251, 250, 249, 248, 247,
+246, 245, 243, 242, 240, 239, 237, 236, 234, 232,
+230, 228, 226, 224, 222, 220, 218, 215, 213, 211,
+208, 206, 203, 201, 198, 195, 193, 190, 187, 184,
+181, 179, 176, 173, 170, 167, 164, 161, 158, 155,
+152, 148, 145, 142, 139, 136, 133, 130, 126, 123,
+120, 117, 114, 111, 108, 104, 101, 98, 95, 92, 
+89, 86, 83, 80, 77, 75, 72, 69, 66, 63, 
+61, 58, 55, 53, 50, 48, 45, 43, 41, 38, 
+36, 34, 32, 30, 28, 26, 24, 22, 20, 19, 
+17, 16, 14, 13, 11, 10, 9, 8, 7, 6,
+5, 4, 3, 3, 2, 2, 1, 1, 0, 0,
+0, 0, 0, 0, 0, 1, 1, 1, 2, 2,
+3, 4, 4, 5, 6, 7, 8, 10, 11, 12, 
+13, 15, 16, 18, 20, 21, 23, 25, 27, 29, 
+31, 33, 35, 37, 39, 42, 44, 47, 49, 51, 
+54, 57, 59, 62, 65, 67, 70, 73, 76, 79, 
+82, 85, 88, 91, 94, 97, 100, 103, 106, 109,
+112, 115, 119, 122, 125, 128  
+};
 byte index = 0;
 unsigned int sample = 0x0;
 
@@ -172,16 +178,9 @@ unsigned int sample = 0x0;
 // the digital audio output port.
 ISR(TIMER2_COMPA_vect) {
   if (isPlaying) {
-    sample = buffer[index];
+    PORTD &= 0x04;
+    PORTD |= (buffer[index]&0xFB);
     index = index + 1;
-    digitalWrite(APORT[0], (sample & 0x1));
-    digitalWrite(APORT[1], (sample & 0x2));
-    digitalWrite(APORT[2], (sample & 0x4));
-    digitalWrite(APORT[3], (sample & 0x8));
-    digitalWrite(APORT[4], (sample & 0x10));
-    digitalWrite(APORT[5], (sample & 0x20));
-    digitalWrite(APORT[6], (sample & 0x40));
-    digitalWrite(APORT[7], (sample & 0x80));
   } else {
     sample = analogRead(MicrophonePin);
     buffer[index] = sample;
@@ -193,10 +192,11 @@ ISR(TIMER2_COMPA_vect) {
 //  seeing if the whole buffer if full of
 //  that button's 2-bit number.
 bool isPressed(int button) {
-  if ((BufferArray & 0xFF00) == (button & 0xFF00)) {
-    BufferArray = 0;
+  if ((ButtonBuffer & 0xFF00) == (button & 0xFF00)) {
+    ButtonBuffer = 0;
     return true;
   } else {
+		if (ButtonBuffer != 0) Serial.println(BufferArray);
     return false;
   }
 }
@@ -216,67 +216,31 @@ long readSample(int n, File f) {
   return sample;
 }
 
+void angryBlink(int pin, int num_blinks, bool end_state) {
+	for (int i=0; i<(2*num_blinks); i++) {
+		end_state = !end_state;
+		digitalWrite(pin, end_state);
+		delay(300);
+	}
+}
+
 void loop() {
-	Serial.print("I HAVE ENTERED THE LOOP!!!!");
+
+	// P L A Y /////////////////////////////////////////////////////////////
   if (isPressed(PLAY)) {
     while (isPressed(PLAY)) {} //Will start playing when button released (Hanger)
     if (haveFile) {
-      digitalWrite(PlayLED, HIGH); // PlayLED on
-      isPlaying = false;
+      digitalWrite(ActiveLED, HIGH); // PlayLED on
       quitPlaying = false;
-      //Timer1.start();
 
-/*
-      byte data = 0;
-      myFile = SD.open("timemachine.wav", FILE_READ);
-      //myFile = SD.open("sine/20000hz.wav", FILE_READ);
-      //myFile = SD.open("krider.wav", FILE_READ);
-      //myFile = SD.open("nocomp.wav", FILE_READ);
-      //myFile = SD.open("ateam.wav", FILE_READ);
-      //myFile = SD.open("test.txt", FILE_READ);
-*/
-      if (myFile) {
-        // print out the header
-        for (int i=0; i<44; i++) {
-          Serial.print(myFile.read());
-        }
-        // play the audio, assuming 24-bit
-        unsigned long sample = 0;
-        
-        int count = 0;
-        boolean derp = true;
-        
-        while (myFile.available()) {
-         //sample = (readSample(3, myFile) * 255) / 65536;
-/*
-				 sample = myFile.read();
-         //Serial.println(sample);
-				 buffer[count] = sample;
-				 if (count == 255) isPlaying = true;
-				 count = (count + 1)%256;
-         //analogWrite(SpeakerPin, sample);
-*/
-         
-         // SINE
-         //analogWrite(SpeakerPin, sine[count]);
-				 buffer[count] = sine[count];
-				 if (count == 100) isPlaying = true;
-         count = (count+1)%101;
-         
-         // SQUARE
-         //if (derp) analogWrite(SpeakerPin, 200);
-         //else analogWrite(SpeakerPin, 0);
-         //derp = !derp;
-        }
-        delay(10000);
-      } else {
-        Serial.println("WTFFFF MANNNN");
-        delay(1000);
-      }
-       
+      //Timer2.start();
+  		TIMSK2 |= (1 << OCIE2A);
+
       while (isPlaying) {
         
         //data = myFile.read();
+				buffer[head]
+      	isPlaying = true;
         
         if (quitPlaying) {
           Serial.print("Press again to quit!");
@@ -286,32 +250,34 @@ void loop() {
           quitPlaying = isPressed(PLAY); //stays true until pressed
         }
       }
-      //Timer1.stop();
-      digitalWrite(PlayLED, LOW); // PlayLED off
+
+      //Timer2.stop();
+			TIMSK2 = 0x0;
+			isPlaying = false;
+      digitalWrite(ActiveLED, LOW); // PlayLED off
+
     } else {
-      digitalWrite(PlayLED, HIGH); //Blink angirly here as well to let the user know
-      delay(300);                  //that there is no file to play, we could remove it
-      digitalWrite(PlayLED, LOW); //if we have time to use LCD
-      delay(300);
-      digitalWrite(PlayLED, HIGH);
-      delay(300);
-      digitalWrite(PlayLED, LOW);
-      delay(300);
-      digitalWrite(PlayLED, HIGH);
-      delay(300);
-      digitalWrite(PlayLED, LOW); //End of blinking
-      
-      
+			angryBlink(ActiveLED, 3, LOW); // User error: nothing to play!
       Serial.print("NO FILE TO PLAY");
       // CONTINUE
     }
-  } else if (isPressed(RECORD)) {
+  } 
+
+	// R E C O R D /////////////////////////////////////////////////////////
+	else if (isPressed(RECORD)) {
     while (isPressed(RECORD)) {} //Will start recodring when button is released
     if (!haveFile) {
-      digitalWrite(RecordLED, HIGH); // RecordLED on (to Ben: Shouldn't be HIGHT?)
+      digitalWrite(ActiveLED, HIGH); // RecordLED on 
       isRecording = true;
       quitRecord = false;
+
+      //Timer2.start();
+  		TIMSK2 |= (1 << OCIE2A);
+
       while (isRecording) {
+
+				// Writing to SD card from buffer! <<< !!!?
+
         if (quitRecord) {
           Serial.print("Press again to quit!");
           isRecording = !isPressed(RECORD); //stays true until pressed
@@ -320,58 +286,42 @@ void loop() {
         quitRecord = isPressed(RECORD);
         }
       }
-      digitalWrite(RecordLED, LOW); // RecordLED off
+
+      //Timer2.stop();
+  		TIMSK2 = 0;
+
+      digitalWrite(ActiveLED, LOW); // RecordLED off
       haveFile = true;
       digitalWrite(FileLED, HIGH); // FileLED on
-      // create file...?
+      // create file...? <<< !!!??
       // CONTINUE
+
     } else {
-      digitalWrite(FileLED, HIGH); //Blink angirly
-      delay(300);                  
-      digitalWrite(FileLED, LOW); 
-      delay(300);
-      digitalWrite(FileLED, HIGH);
-      delay(300);
-      digitalWrite(FileLED, LOW);
-      delay(300);
-      digitalWrite(FileLED, HIGH);
-      delay(300);
-      digitalWrite(FileLED, LOW); 
-      delay(300);
-      digitalWrite(FileLED, HIGH);//End of blinking
-      
+			angryBlink(FileLED, 3, HIGH); // User error: already have file!
       Serial.print("WE ALREADY HAVE A FILE");
       // CONTINUE
     }
-  } else if (isPressed(DELETE)) {
+  } 
+
+	// D E L E T E /////////////////////////////////////////////////////////
+	else if (isPressed(DELETE)) {
     if (haveFile) {
-      // delete the file somehow
+      // delete the file somehow <<< !!!
       haveFile = false;
       digitalWrite(FileLED, LOW); // FileLED off
+
     } else {
-      digitalWrite(FileLED, HIGH); //Blink angirly
-      delay(300);                  
-      digitalWrite(FileLED, LOW); 
-      delay(300);
-      digitalWrite(FileLED, HIGH);
-      delay(300);
-      digitalWrite(FileLED, LOW);
-      delay(300);
-      digitalWrite(FileLED, HIGH);
-      delay(300);
-      digitalWrite(FileLED, LOW);  //End of blinking
-      
+			angryBlink(FileLED, 3, LOW); // User error: nothing to delete!
       Serial.print("NO FILE TO DELETE");
       // CONTINUE
     }
+
+	// n o t h i n g . . . /////////////////////////////////////////////////
   } else {
     // No buttons were pressed. GREAT!
-    //Serial.println(BufferArray);
-    BufferArray = 0;
-    Serial.println("No buttons pressed...");
+    ButtonBuffer = 0;
     // CONTINUE
   }
-  //delay(100);
 }
 
 
